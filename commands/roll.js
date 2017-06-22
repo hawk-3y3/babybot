726f8541307b47536524d799e1473542bef97a88
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-06-22 19:34:16 +0200

    sloppy working version of the roll command made

--- commands/roll.js
+++ commands/roll.js
@@ -1,34 +1,90 @@
 exports.run = (client, message, args) => {
-    let result = "You rolled ";
     let input = (args[0] == null) ? ['1d6'] : args;
-    let dice
-    let i
+    let data
     
-    for (var index = 0; index < input.length; index++) {
-        var element = input[index];   
-        i = 0
-        dice = /(\d+)[d,D](\d+)/g.exec(element)
+    data = rollDie(input)
 
-        if (dice == null) {
-            dice = /[d,D](\d+)/g.exec(element)
-            dice.splice(1, 0, 1);
-        }
-
-        if(dice[1] == 1){
-            result += "a "
-        }
-        while (i < dice[1]) {
-            i++
-            result += Math.ceil(Math.random() * dice[2])
-            if (i !== dice[1]){
-                result += ", "
-            }
-        }
+    if (data == null){
+        message.channel.send(`invalid die format, try 1d6`).catch(console.error);
+        return
     }
     
-    message.channel.send(result).catch(console.error);
+    message.channel.send(`You rolled ${data}.`).catch(console.error);
+    
+    data = multiCheck(data)
+
+    if(!data){
+        return
+    } else {
+    message.channel.send(`total: ${data} !`).catch(console.error);
+    }
 }
 
 exports.help = () => {
-    return "(number of dice)d(size of die)\n    rolls dice for you\n    Adding nDn will roll that many dice.\n"
+    return "(number of dice)d(size of die)\n    rolls a d6 for you\n    Adding nDn will roll that many dice of that type.\n"
+}
+
+function rollDie(input) {
+    let data = ""
+    let dice
+    let i
+
+    for (var index = 0; index < input.length; index++) {
+        var element = input[index];   
+        i = 0
+
+        dice = resolveDie(element)
+
+        if (dice == null){
+            return
+        }
+
+        while (i < dice.dice[1]) {
+            i++
+            data += Math.ceil(Math.random() * dice.dice[2])
+            if (i != dice.dice[1]){
+                data += ", "
+            }
+        }
+        if (index + 1 < input.length){
+            data += ", "
+        }
+    }
+    return data
+}
+
+function resolveDie(die) {
+    let dieData = {
+        text: ""
+    }
+
+    dieData.dice = /(\d+)[d,D](\d+)/g.exec(die)
+
+    if (dieData.dice == null) {
+        dieData.dice = /[d,D](\d+)/g.exec(die)
+        if (dieData.dice == null) {
+            return null
+        }
+        dieData.dice.splice(1, 0, 1);
+    }
+
+    console.log(dieData)
+
+    return(dieData)
+}
+
+function multiCheck(input){
+    let numbers = input.split(',').map(function(item) {
+        return parseInt(item, 10);
+    });
+
+    if (numbers.length > 1) {        
+        return sum(numbers)
+        
+    }
+    return false
+}
+
+function sum(numbers){
+    return numbers.reduce(function (a, b) { return a + b; }, 0);
 }
