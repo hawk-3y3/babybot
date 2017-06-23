a6d9986d77625b72494068d22c14512c72753f8a
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-06-24 00:16:58 +0200

    rewritten, thanks mons

--- commands/roll.js
+++ commands/roll.js
@@ -1,90 +1,29 @@
-exports.run = (client, message, args) => {
-    let input = (args[0] == null) ? ['1d6'] : args;
-    let data
-    
-    data = rollDie(input)
+exports.run = function (client, msg, args) {
 
-    if (data == null){
-        message.channel.send(`invalid die format, try 1d6`).catch(console.error);
-        return
-    }
-    
-    message.channel.send(`You rolled ${data}.`).catch(console.error);
-    
-    data = multiCheck(data)
+    let rx = /((?:\d*)d(?:\d+))/gi;
+    let rx2 = /^(?:(\d*)d(\d+))/i
+    let r = rx.test(args.join(' ')) ? args.join(' ').match(rx) : ['d20'];
+    let result = new Array();
+    let sum;
 
-    if(!data){
-        return
-    } else {
-    message.channel.send(`total: ${data} !`).catch(console.error);
-    }
+    r.forEach(d => {
+        let data = d.match(rx2);
+        let rolls = (/\d+/).test(data[1]) ? data[1] : 1;
+
+        for (i = 0;i < rolls;i++){
+            let res = Math.ceil(Math.random() * data[2]);
+            result.push(res);
+        }
+
+    });
+    sum = result.reduce((a, b) => a + b, 0);
+    msg.channel.send({ embed: {
+        color: 4492543,
+        title: "Dice Roll",
+        description: `Your rolls: ${result.join(', ')}.\nTotal sum: ${sum}.`
+    }})
+
 }
-
 exports.help = () => {
     return "(number of dice)d(size of die)\n    rolls a d6 for you\n    Adding nDn will roll that many dice of that type.\n"
-}
-
-function rollDie(input) {
-    let data = ""
-    let dice
-    let i
-
-    for (var index = 0; index < input.length; index++) {
-        var element = input[index];   
-        i = 0
-
-        dice = resolveDie(element)
-
-        if (dice == null){
-            return
-        }
-
-        while (i < dice.dice[1]) {
-            i++
-            data += Math.ceil(Math.random() * dice.dice[2])
-            if (i != dice.dice[1]){
-                data += ", "
-            }
-        }
-        if (index + 1 < input.length){
-            data += ", "
-        }
-    }
-    return data
-}
-
-function resolveDie(die) {
-    let dieData = {
-        text: ""
-    }
-
-    dieData.dice = /(\d+)[d,D](\d+)/g.exec(die)
-
-    if (dieData.dice == null) {
-        dieData.dice = /[d,D](\d+)/g.exec(die)
-        if (dieData.dice == null) {
-            return null
-        }
-        dieData.dice.splice(1, 0, 1);
-    }
-
-    console.log(dieData)
-
-    return(dieData)
-}
-
-function multiCheck(input){
-    let numbers = input.split(',').map(function(item) {
-        return parseInt(item, 10);
-    });
-
-    if (numbers.length > 1) {        
-        return sum(numbers)
-        
-    }
-    return false
-}
-
-function sum(numbers){
-    return numbers.reduce(function (a, b) { return a + b; }, 0);
 }
