57a1089b1c6cbe64e98ab3d8c923587903d237e2
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-07-11 21:20:28 +0200

    small fixes to commands that use the prefix

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,15 @@
 fsutil = require('../utilities/fsutil.js')
 
 exports.run = async (client, message, args) => {
+if(!args[0]){
+    message.channel.send({embed:{
+        title: `current prefix`,
+        description: `the prefix is currently: "${client.prefixes[message.guild.id]}".`
+    }})
+}
+
+
+
 client.prefixes[message.guild.id] = args[0];
 	await fsutil.fsWriteFile(__dirname + '/../data/prefixes.json', JSON.stringify(client.prefixes, null, 4))
 
