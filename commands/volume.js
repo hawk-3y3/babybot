ec230b4a40e50a8de19ebda35a7e5170d20b21b0
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-07-27 13:44:28 +0200

    changes

--- commands/volume.js
+++ commands/volume.js
@@ -2,6 +2,7 @@ const fsutil = require('../utilities/fsutil.js')
 
 
 exports.run = async (message, args, options) => {
+    
     if ( !(permissions.isDJ(message.member, client) || permissions.isAdmin(message.member)) ) return message.channel.send({ embed: {
 		color: client.config.options.embedColour,
 		title: "Access Denied",
@@ -18,7 +19,7 @@ exports.run = async (message, args, options) => {
     }    
 
     if(parseInt(args[0])){
-        if(!options.includes("--overload")(parseInt(args[0]) > 200 || parseInt(args[0]) < 0)){
+        if((parseInt(args[0]) > 200 || parseInt(args[0]) < 0)){
             message.channel.send({embed:{
                 title: `out of range`,
                 color: client.config.options.embedColour,
